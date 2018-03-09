4c43edb4921e0ffabd9d8a94a43cd591be41a371
farahkhancode <farahmed@gmail.com>
2018-03-09 10:51:04 -0600

    complete

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
          <nav>
             <Link to='/'>Landing</Link>
             <Link to='/library'>Library</Link>
-            
+
 
         </nav>
             <h1>Bloc Jams</h1>
