c0f63b1383fbdab71e4ce67f450d8e36476533d5
farahkhancode <farahmed@gmail.com>
2018-03-12 10:03:39 -0500

    Album CP complete

--- src/App.js
+++ src/App.js
@@ -14,8 +14,6 @@ class App extends Component {
          <nav>
             <Link to='/'>Landing</Link>
             <Link to='/library'>Library</Link>
-
-
         </nav>
             <h1>Bloc Jams</h1>
       </header>
