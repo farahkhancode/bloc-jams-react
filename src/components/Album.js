0c110572f54881d1d71596f417720790eae899ab
farahkhancode <farahmed@gmail.com>
2018-03-13 15:50:30 -0500

    clearing worktree

--- src/components/Album.js
+++ src/components/Album.js
@@ -11,9 +11,40 @@ class Album extends Component {
 });
 
 this.state = {
-  album: album
+  album: album,
+  currentSong: album.songs[0],
+  isPlaying: false
 };
+
+this.audioElement = document.createElement('audio');
+this.audioElement.src = album.songs[0].audioSrc;
   }
+
+play() {
+     this.audioElement.play();
+     this.setState({ isPlaying: true });
+   }
+
+pause() {
+       this.audioElement.pause();
+       this.setState({ isPlaying: false });
+     }
+
+setSong(song) {
+          this.audioElement.src = song.audioSrc;
+          this.setState({ currentSong: song });
+        }
+
+handleSongClick(song) {
+      const isSameSong = this.state.currentSong === song;
+      if (this.state.isPlaying && isSameSong) {
+        this.pause();
+      } else {
+         if (!isSameSong) { this.setSong(song); }
+        this.play();
+         }
+          }
+
 
   render() {
     return (
@@ -34,13 +65,17 @@ this.state = {
           </colgroup>
           <tbody className ="songs-list">
           { this.state.album.songs.map(( song, index ) =>
-          <tr className="song" key={index}>
+          <tr className="song" key={index} onClick={() => this.handleSongClick(song)} >
           <td className="song-actions">
-             <button>
-               <span className="song-number">{(index+1) + ' '}</span>
-               <span className="ion-play"></span>
-               <span className="ion-pause"></span>
-             </button>
+          <button>
+             <span className="song-number">{(index+1) + ' '}</span>
+          </button>
+          <button>
+             <span className="ion-play"></span>
+          </button>
+          <button>
+             <span className="ion-pause"></span>
+          </button>
           </td>
           <td className="song-title">{song.title}</td>
           <td className="song-duration">{song.duration}</td>
